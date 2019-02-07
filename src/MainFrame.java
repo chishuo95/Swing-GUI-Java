04a1a5aa68b727d542bc8704d84b3f0dad351f75
chishuo <chishuo2017@163.com>
2019-02-07 11:55:02 -0500

    Lecture 2 : Hello world Creating a basic Swing application JFrame Lecture 3 : Layout, text, button BorderLayout JTextArea JButton Lecture 4 :responding to button clicks Lecture 5 :custom components JPanel JScrollPanel

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,15 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame{
-    private JTextArea textArea;
     private JButton btn;
+    private TextPanel textPanel;
     public MainFrame(){
         super("Hello World!");
-        setLayout(new BorderLayout( ));
-        textArea = new JTextArea();
+        setLayout(new BorderLayout());
+        textPanel = new TextPanel();
         btn = new JButton("Click Me!");
-        add(textArea,BorderLayout.CENTER);
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textPanel.appendText("Hello\n");
+            }
+        });
+        add(textPanel,BorderLayout.CENTER);
         add(btn,BorderLayout.SOUTH);
         setSize(600,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//click exit Jframe stop running
